8a8447346792069340cbbdc5f02d33575c814d6e
ecigwegbu <igwegbu@gmail.com>
2023-08-03 12:44:40 +0100

    docs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,7 +16,7 @@ from typing import List
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
