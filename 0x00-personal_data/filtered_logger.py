33bdd65f3f1b831f7047b7ebb70f8d95021adf61
ecigwegbu <igwegbu@gmail.com>
2023-08-03 11:06:16 +0100

    0-Regex-ing -docstrings edited

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """This module contains a function that obfucates a log message."""
+
+import logging
 import re
 from typing import List
-import logging
 
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
@@ -22,22 +23,17 @@ class RedactingFormatter(logging.Formatter):
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """Write a function called filter_datum that returns the log message
-    obfuscated:
+    """
+    Return an obfuscated log message.
 
-    Arguments:
-    fields: a list of strings representing all fields to obfuscate
-    redaction: a string representing by what the field will be obfuscated
-    message: a string representing the log line
-    separator: a string representing by which character is separating all
-    fields in the log line (message)
-    The function should use a regex to replace occurrences of certain
-    field values.
-    filter_datum should be less than 5 lines long and use re.sub to perform
-    the substitution with a single regex."""
+    Keyword arguments:
+    fields -- a list of fields to obfusecate
+    redaction: -- the string to replace the fields with
+    message -- the log message
+    """
 
     for field in fields:
-        out_text = "{}{}=[^;]*{}".format(separator, field, separator)
-        in_text = "{}{}={}{}".format(separator, field, redaction, separator)
+        out_text: str = "{}{}=[^;]*{}".format(separator, field, separator)
+        in_text: str = "{}{}={}{}".format(separator, field, redaction, separator)
         message = re.sub(out_text, in_text, message)
     return message
