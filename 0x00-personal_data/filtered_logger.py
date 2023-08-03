14c5f8f40d0ffad6cb6e4298d35a2fef313f7c6c
ecigwegbu <igwegbu@gmail.com>
2023-08-03 13:19:21 +0100

    0. Regex-ing - no separator in field'

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,13 +1,47 @@
 #!/usr/bin/env python3
-""" Main file - functions that manage PII."""
+"""
+    Main file - functions that manage PII.
+
+    Classes:
+        RedactionFormatter(logging.Formatter)
+    methods:
+        filter_datum(fields: List[str], redaction: str,
+                     message: str, separator: str) -> str:
+    """
+
 import logging
 import re
 from typing import List
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        """Define the object using the provided arguments"""
+
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the message and return a formatted version"""
+        NotImplementedError
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """Return an obfuscated log message."""
+    """ Return an obfuscated log message.
+
+        Keyword arguments:
+        fields: List[str] -- a list of fields to obfusecate
+        redaction: str -- the string to replace the fields with
+        message: str -- the log message
+        """
+
     for field in fields:
         ouTx: str = "{}=[^{}]*".format(field, separator)
         inTx: str = "{}={}".format(field, redaction)
