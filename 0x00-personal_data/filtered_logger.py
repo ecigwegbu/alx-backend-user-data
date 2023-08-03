7c2f74c30a4c6361f8394800797f7a2ce87cd644
ecigwegbu <igwegbu@gmail.com>
2023-08-03 12:46:09 +0100

    docs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,27 +7,11 @@
     methods:
         filter_datum(fields: List[str], redaction: str,
                      message: str, separator: str) -> str:
-    """
+"""
 
 import logging
 import re
 from typing import List
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        """Define the object using the provided arguments"""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        """Format the message and return a formatted version"""
-        NotImplementedError
 
 
 def filter_datum(fields: List[str], redaction: str,
