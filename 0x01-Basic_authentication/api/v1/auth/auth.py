58363979b69055c65fa04637db56fd642e5c3ae2
ecigwegbu <igwegbu@gmail.com>
2023-08-08 11:06:09 +0100

    4. Define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,8 +13,9 @@ class Auth():
         """Return False - path and excluded_paths will be used later
         now, you don’t need to take care of them.
         """
-        if excluded_paths and (path in excluded_paths or
-                               (path + "/") in excluded_paths):
+        # if excluded_paths and (path in excluded_paths or
+        if path and excluded_paths and (path in excluded_paths or
+                                        (path + "/") in excluded_paths):
             return False
         return True
 
