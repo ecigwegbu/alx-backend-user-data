0f27a801e48009ddd79a04e5dfeef3fc19b84488
ecigwegbu <igwegbu@gmail.com>
2023-08-08 19:16:26 +0100

    6. Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
-""" Task Module
+""" Basic Auth Class - Inherits from Basic Authentication
 """
+from api.v1.auth.auth import Auth
+from typing import List, Union
+
+
+class BasicAuth(Auth):
+    """ Manage the API Authentication
+    """
+    pass
