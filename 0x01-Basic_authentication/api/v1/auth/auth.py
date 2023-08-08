bcf5f1de85b9359dbefb5a59782f723822b9a01e
ecigwegbu <igwegbu@gmail.com>
2023-08-08 23:50:21 +0100

    13. Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, Union, TypeVar
+from fnmatch import fnmatch
 
 
 class Auth():
@@ -13,8 +14,9 @@ class Auth():
         """Return False - path and excluded_paths will be used later
         now, you don’t need to take care of them.
         """
-        if path and excluded_paths and (path in excluded_paths or
-                                        (path + "/") in excluded_paths):
+        # if path and excluded_paths and (path in excluded_paths or
+        #                                 (path + "/") in excluded_paths):
+        if path and excluded_paths and path_matches(path, excluded_paths):
             return False
         return True
 
@@ -29,3 +31,11 @@ class Auth():
         """Return None - None - request will be the Flask request object
         """
         return None
+
+    def path_matches(path: str, patterns: List[str]) -> bool:
+        """match pathname against a pattern
+        """
+        for pattern in patterns:
+            if fnmatch(path, pattern) or fnmatch(path + "/", pattern):
+                return True
+        return False
