13311fb8299a642853552ef7327f306c89b0f3d6
ecigwegbu <igwegbu@gmail.com>
2023-08-08 21:00:00 +0100

    task7

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,7 +19,7 @@ auth = None
 if getenv('AUTH_TYPE') == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif getenv('AUTH_TYPE') == 'auth':  # debug: try without elif
+elif getenv('AUTH_TYPE') == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
 
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,36 @@
 """ Basic Auth Class - Inherits from Basic Authentication
 """
 from api.v1.auth.auth import Auth
-from typing import List, Union
+from base64 import b64decode
+from typing import Union
 
 
 class BasicAuth(Auth):
     """ Manage the API Authentication
     """
     pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) \
+            -> Union[str, None]:
+        """ Return the Base64 part of the Authorization header for a Basic
+            Authentication
+        """
+        base64_authorization_header = None
+        if authorization_header and type(authorization_header) == str and \
+                authorization_header.startswith("Basic "):
+            base64_authorization_header = authorization_header[6:].strip()
+        return base64_authorization_header
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> Union[str, None]:
+        """ Return the decoded value of a Base64 string
+            base64_authorization_header
+        """
+        result = None  # 'username:password'
+        if base64_authorization_header and \
+                type(base64_authorization_header) == str:
+            try:
+                result = b64decode(base64_authorization_header).decode('utf-8')
+            except UnicodeDecodeError:
+                result = None
+        return result
