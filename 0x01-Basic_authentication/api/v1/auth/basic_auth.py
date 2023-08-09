ca7ac3e40375df7312133b3d6064f00d388fda52
ecigwegbu <igwegbu@gmail.com>
2023-08-10 00:25:17 +0100

    debug current_user_basic

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -72,10 +72,13 @@ class BasicAuth(Auth):
         """
         try:
             auth_header = self.authorization_header(request)
-            base64_header = extract_base64_authorization_header(auth_header)
-            decoded_header = decode_base64_authorization_header(base64_header)
-            user_email, user_pwd = extract_user_credentials(decoded_header)
-            user = user_object_from_credentials(user_email, user_pwd)
+            base64_header = \
+                self.extract_base64_authorization_header(auth_header)
+            decoded_header = \
+                self.decode_base64_authorization_header(base64_header)
+            user_email, user_pwd = \
+                self.extract_user_credentials(decoded_header)
+            user = self.user_object_from_credentials(user_email, user_pwd)
         except Exception:
             return None
         return user
