f9d39c611347f140b4bb8f15146081eec897db4d
ecigwegbu <igwegbu@gmail.com>
2023-08-10 23:45:01 +0100

    2. Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,9 +5,19 @@ from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
 from typing import Union, TypeVar
+import uuid
 
 
 class SessionAuth(Auth):
     """ Manage the API Authentication using Session object
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id
+        """
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        user_id_by_session_id[session_id] = user_id
+        return session_id
