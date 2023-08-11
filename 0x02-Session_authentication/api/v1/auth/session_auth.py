44759ddf6deceaac141fa2d4cc23e292466202a5
ecigwegbu <igwegbu@gmail.com>
2023-08-11 01:04:54 +0100

    2. Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -19,5 +19,5 @@ class SessionAuth(Auth):
         if user_id is None or type(user_id) != str:
             return None
         session_id = str(uuid.uuid4())
-        user_id_by_session_id[session_id] = user_id
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
