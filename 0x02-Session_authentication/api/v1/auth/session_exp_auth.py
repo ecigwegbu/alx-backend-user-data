f43d3c0f5fbe1b77bf27fb41738ae4ebbbf5b00a
ecigwegbu <igwegbu@gmail.com>
2023-08-12 18:22:47 +0100

    9. Expiration?

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -2,6 +2,8 @@
 """Setup data files"""
 from api.v1.auth.session_auth import SessionAuth
 from models.user import User
+from datetime import datetime, timedelta
+import os
 
 
 class SessionExpAuth(SessionAuth):
@@ -12,16 +14,40 @@ class SessionExpAuth(SessionAuth):
         """
         env = os.getenv("SESSION_DURATION")
         try:
-            session_duration = int(env)
+            self.session_duration = int(env)
         except Exception:
-            session_duration = 0
+            self.session_duration = 0
 
-    def create_session(self, user_id=None):
-        """Create a session ID for a given user_id
+    def create_session(self, user_id=None) -> str:
+        """Create a session ID for a given user_id and make an entry with
+            that session_id as key in the user_id_by_session_id dictionary
         """
-        pass
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+        session_dictionary = {
+                'user_id': user_id,
+                'created_at': datetime.now()
+        }
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
 
-    def user_id_for_session_id(self, session_id=None):
-        """Lookup user_id based on a session_id
+    def user_id_for_session_id(self, session_id=None) -> str:
+        """Lookup user_id based on a session_id, return a valid user_id
+            if it has not expired, otherwise return None
         """
-        pass
+        if not session_id:
+            return None
+        session_dictionary = self.user_id_by_session_id.get(session_id)
+        if not session_dictionary:
+            return None
+        user_id = session_dictionary.get('user_id')
+        created_at = session_dictionary.get('created_at')
+        if self.session_duration <= 0:
+            return user_id
+        if created_at is None:
+            return None
+        if created_at + timedelta(seconds=self.session_duration) < \
+                datetime.now():
+            return None
+        return user_id
