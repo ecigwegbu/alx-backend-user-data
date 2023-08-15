e920f4a3f76da3c6f5660292687e68fcca1c3720
ecigwegbu <igwegbu@gmail.com>
2023-08-15 18:21:14 +0100

    6.basic Flask App

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
-"""Main entry point into Flask App.
 """
+Basic Flask app that returns a BienVenue message
+"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def root_route() -> str:
+    """ GET Home Page of the app
+    Return:
+        - Welcome Page
+    """
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
