f9ce01b4af58a83ed786525213c96b8fe87dc9d0
ecigwegbu <igwegbu@gmail.com>
2023-08-15 21:21:21 +0100

    7. Register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,19 +2,40 @@
 """
 Basic Flask app that returns a BienVenue message
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'], strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def root_route() -> str:
     """ GET Home Page of the app
     Return:
         - Welcome Page
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users():
+    """Implements the /users POST route
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        registered_user = AUTH.register_user(email, password)
+        if registered_user:
+            return jsonify({"email": email, "message": "user created"}), 200
+        else:
+            return jsonify({"message": "bad request"}), 400
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    pass
 
 
 if __name__ == "__main__":
