9bb0a47456effac6d94a094383e758977f3fac0a
ecigwegbu <igwegbu@gmail.com>
2023-08-15 17:21:28 +0100

    4. Hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 """Authorization module for the Flask App
 """
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Return a hashed version of a password
+    """
+    if type(password) != str:
+        return None
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
